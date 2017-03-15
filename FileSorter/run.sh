552b4eb0a5a4f8eaa3a9d4e91d78360c634f6a6e
Yanano <ymuruvi@gmail.com>
2017-03-15 01:29:16 -0400

    Added starting and ending remarks

--- FileSorter/run.sh
+++ FileSorter/run.sh
@@ -1,3 +1,4 @@
+echo STARTING
 perl sortfiles.pl \#02520051-eng.csv Data 240000 0 yes
 perl sortfiles.pl \#02520051-eng.csv Data 480000
 perl sortfiles.pl \#02520051-eng.csv Data 720000
@@ -8,3 +9,4 @@ perl sortfiles.pl \#02520051-eng.csv Data 1680000
 perl sortfiles.pl \#02520051-eng.csv Data 1920000
 perl sortfiles.pl \#02520051-eng.csv Data 2160000
 perl sortfiles.pl \#02520051-eng.csv Data 2400000
+echo DONE
