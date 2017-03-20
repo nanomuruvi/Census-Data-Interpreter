c140c7a3adc2c2798e693b708fee337232c1362a
Yanano Muruvi <ymuruvi@mail.uoguelph.ca>
2017-03-20 13:35:59 -0400

    Boxplots work now

---
+++ Graphing/formatData.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+use version; our $VERSION = qv('5.16.0');
+use Statistics::R;
+use Text::CSV  1.32;
+
+my $EMPTY	= q{};
+my $SPACE	= q{ };
+my $COMMA	= q{,};
+
+my $csv		= Text::CSV->new({ sep_char => $COMMA });
+
+#The outputPDF and inputData files are pretty self explanatory.
+#The plotType refers to the specific plot which shall be made, these include: "boxplot"
+my $outputPDF;
+my $inputData;
+my $plotType;
+
+
+if ($#ARGV != 2 ) {
+   print "Usage: plotNames.pl <input file name> <pdf file name> <plot type>\n" or
+      die "Print failure\n";
+   exit;
+} else {
+	$inputData	= $ARGV[0];
+	$outputPDF 	= $ARGV[1];
+	$plotType	= $ARGV[2];
+}
+
--- Graphing/graphData.pl
+++ Graphing/graphData.pl
@@ -8,14 +8,22 @@ my $EMPTY	= q{};
 my $SPACE	= q{ };
 my $COMMA	= q{,};
 
+my $R = Statistics::R->new();
 my $csv		= Text::CSV->new({ sep_char => $COMMA });
 
-#The outputPDF and inputData files are pretty self explanatory.
-#The plotType refers to the specific plot which shall be made, these include: "boxplot"
+=begin comment
+	The outputPDF and inputData files are pretty self explanatory.
+	The plotType refers to the specific plot which shall be made, these include: "boxplot"
+	The inputData must be columns of data with a location as the column header with the 
+	location data underneath in a numerical format. If there is no value it should be left empty
+	Data values should remain in their respective columns
+=end comment
+=cut
 my $outputPDF;
 my $inputData;
 my $plotType;
-
+my $xLab;
+my $yLab;
 
 if ($#ARGV != 2 ) {
    print "Usage: plotNames.pl <input file name> <pdf file name> <plot type>\n" or
@@ -27,36 +35,25 @@ if ($#ARGV != 2 ) {
 	$plotType	= $ARGV[2];
 }
 
-my $R = Statistics::R->new();
-
-my $RCommand;
-
 $R -> run(qq`pdf("$outputPDF" , paper="letter")`);
 $R -> run(q`library(ggplot2)`);
 
 if($plotType eq"boxplot"){
-	$RCommand = boxplot();
+	boxplot($inputData);
 }else{
-	$RCommand = q`boxplot(c(1,2,3,4,4,4,5,6,7,8,9))`;
 }
 
-#TODO: Need to add takign of input and allow for multiple datasets
-$R -> run($RCommand);
-
 $R -> run(q`dev.off()`);
+
 $R -> stop();
 
-#Good R boxplot tutorial: https://www.youtube.com/watch?v=U64yNvlhv9I
 sub boxplot{
-	my $R = $_[0];
-	my $file = $_[1];
-	my $command;
-	$R -> run(q`data <- read.csv('file')`);
-	$R -> run(q`boxplot()`);
-	return $command;
+	my $file = $_[0];
+	$R -> run(qq`data <- read.csv("$file")`);
+	$R -> run(q`attach(data)`);
+	$R -> run(q`boxplot(data)`);
 }
 
 sub barGraph{
 	my $command;
-	return $command;
 }
